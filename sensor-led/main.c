ab64a6497587d51c26393ea9f5f5ab2b7b6b5616
sebastianAng <92903105+sebastianAng@users.noreply.github.com>
2022-01-18 00:09:16 -0300

    mensaje ejemplo sensor led

---
+++ sensor-led/main.c
@@ -0,0 +1,29 @@
+int valor_sensor = 0;
+int led_azul = 13;
+int led_amarillo = 12;
+
+void setup() {
+  pinMode(led_azul, OUTPUT);
+  pinMode(led_amarillo, OUTPUT); 
+  Serial.begin(9600);    // inicia el puerto Serial
+  pinMode(A0, INPUT);     // Pin A0 configurado como entrada
+  pinMode(A1, OUTPUT);     // Pin A1 configurado como salida
+  digitalWrite(A1, HIGH);     // Pin A1 en alto
+
+}
+
+void loop() {
+
+  valor_sensor = analogRead(A0);     // Lectura del pin A0
+  Serial.println(valor_sensor);
+
+  delay(500);
+  
+  if (valor_sensor >= 400) {
+    digitalWrite(led_azul, HIGH);
+    digitalWrite(led_amarillo, LOW);
+  } else {
+    digitalWrite(led_azul, LOW);
+    digitalWrite(led_amarillo, HIGH);
+  }
+}
